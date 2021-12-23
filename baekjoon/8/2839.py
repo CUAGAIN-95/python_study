c962d8e691c2981892b9ae2f254cca01fc1e86a0
yeonhlee <lyhldy@naver.com>
2021-12-24 00:40:28 +0900

    baekjoon 8 2839 설탕 배달

--- baekjoon/8/2839.py
+++ baekjoon/8/2839.py
@@ -1,7 +1,15 @@
 n = int(input())
 
-x5 = n // 5
-
-for i in range(x5, 0, -1):
-    if n % (5 * i)
-
+i = n // 5
+if n % 5 == 0:
+    print(i)
+    exit()
+x3 = 0
+for x5 in range(i, -1, -1):
+    if (n - (5 * x5)) % 3 == 0:
+        x3 = (n - (5 * x5)) // 3
+        break
+if x3 == 0:
+    print(-1)
+else:
+    print(x5 + x3)
