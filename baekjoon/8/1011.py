2500dcfeb0da0da005896286fbed81e38b37b38d
yeonhlee <lyhldy@naver.com>
2021-12-27 15:34:43 +0900

    baekjoon 8 1011 Fly me to the Alpha Centauri 수식 공부 코드 간략화

--- baekjoon/8/1011.py
+++ baekjoon/8/1011.py
@@ -1,9 +1,14 @@
 import math
 
-# n = int(input())
+n = int(input())
 
-# for i in range(n):
-a, b = map(int, input().split())
-d = b - a
-max_move = math.sqrt(d)
-print(math.sqrt(d), math.ceil(max_move), math.floor(max_move))
+for i in range(n):
+    a, b = map(int, input().split())
+    d = b - a
+    max_move = math.sqrt(d)
+    if max_move == math.ceil(max_move):
+        print(int(max_move * 2 - 1))
+    elif d <= math.ceil(max_move) * math.floor(max_move):
+        print(math.floor(max_move) * 2)
+    else:
+        print(math.floor(max_move) * 2 + 1)
