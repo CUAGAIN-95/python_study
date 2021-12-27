88133205a235e2508695b833f40246fea3ded023
yeonhlee <lyhldy@naver.com>
2021-12-27 16:15:22 +0900

    baekjoon 8 1011 Fly me to the Alpha Centauri 코드 간략화

--- baekjoon/8/1011.py
+++ baekjoon/8/1011.py
@@ -1,14 +1,12 @@
-import math
-
 n = int(input())
 
 for i in range(n):
     a, b = map(int, input().split())
     d = b - a
-    max_move = math.sqrt(d)
-    if max_move == math.ceil(max_move):
-        print(int(max_move * 2 - 1))
-    elif d <= math.ceil(max_move) * math.floor(max_move):
-        print(math.floor(max_move) * 2)
+    max_move = int(d ** 0.5)
+    if max_move == d ** 0.5:
+        print(max_move * 2 - 1)
+    elif d <= max_move * (max_move + 1):
+        print(max_move * 2)
     else:
-        print(math.floor(max_move) * 2 + 1)
+        print(max_move * 2 + 1)
