eaa502a596e0ce8e4f0d2cc64ad88aa09ed52a4f
yeonhlee <lyhldy@naver.com>
2022-01-11 10:15:50 +0900

    baekjoon 11 2798 블랙잭

--- baekjoon/11 브루트 포스/2798.py
+++ baekjoon/11 브루트 포스/2798.py
@@ -1,1 +1,11 @@
-n = int(input())
+N, M = map(int, input().split())
+
+arr = list(map(int, input().split()))
+arr.sort()
+sum_max = 0
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            if sum_max < arr[i] + arr[j] + arr[k] <= M:
+                sum_max = arr[i] + arr[j] + arr[k]
+print(sum_max)
