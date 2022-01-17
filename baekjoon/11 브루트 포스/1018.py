656d0aa45cb0ede063e235bd678f6dad53bc5ead
yeonhlee <lyhldy@naver.com>
2022-01-17 17:41:48 +0900

    baekjoon 11 1017 체스판 다시 칠하기 성공!

--- baekjoon/11 브루트 포스/1018.py
+++ baekjoon/11 브루트 포스/1018.py
@@ -2,37 +2,36 @@ import sys
 
 M, N = map(int, sys.stdin.readline().split())
 
-color = [""] * N
+color = [""] * M
 check = [[0] * N for _ in range(M)]
 
 for i in range(M):
     color[i] = sys.stdin.readline()
 
-main_color = color[0][0]
-if main_color == 'B':
-    other_color = 'W'
-else:
-    other_color = 'B'
+first_color = color[0][0]
 
 for i in range(M):
     for j in range(N):
         now_color = color[i][j]
         if (i + j) % 2 == 0:
-            if main_color != color[i][j]:
+            if first_color != color[i][j]:
                 check[i][j] = 1
         else:
-            if other_color != color[i][j]:
+            if first_color == color[i][j]:
                 check[i][j] = 1
 
 min_check = -1
 for i in range(M - 7):
     for j in range(N - 7):
-        count = 0
+        count_0 = 0
+        count_1 = 0
         for x in range(8):
             for y in range(8):
                 if check[i + x][j + y] == 1:
-                    count += 1
-        if min_check == -1 or min_check > count:
-            min_check = count
+                    count_1 += 1
+                else:
+                    count_0 += 1
+        if min_check == -1 or min_check > min(count_0, count_1):
+            min_check = min(count_0, count_1)
 
 print(min_check)
