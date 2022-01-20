23ec0e3f7b1277c686c58adf1495e1fc19f7aede
yeonhlee <lyhldy@naver.com>
2022-01-20 22:01:55 +0900

    baekjoon 12 2108 통계학 시간초과 최적화 필요

--- baekjoon/12 정렬/2108.py
+++ baekjoon/12 정렬/2108.py
@@ -17,10 +17,25 @@ for i in range(n):
 print(my_round(sum(nums), n))
 nums.sort()
 print(nums[int((n - 1) / 2)])
-
-count = 1
-
-
+max_count = 0
+min2 = 0
+mode_num = 0
+for i in range(n):
+    count = 1
+    for j in range(i + 1, n):
+        if nums[i] == nums[j]:
+            count += 1
+        else:
+            break
+    if max_count < count:
+        mode_num = nums[i]
+        max_count = count
+        min2 = 0
+    elif max_count == count:
+        if min2 == 0:
+            min2 = 1
+            mode_num = nums[i]
+    i += count
+print(mode_num)
 range_num = nums[n - 1] - nums[0]
-
 print(range_num)
