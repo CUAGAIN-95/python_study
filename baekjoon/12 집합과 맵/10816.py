89daaf03cf499a5861814ef290d3d665519e5f4b
yeonhlee <lyhldy@naver.com>
2022-08-26 03:44:32 +0900

    baekjoon 12 10816 숫자 카드 2

---
+++ baekjoon/12 집합과 맵/10816.py
@@ -0,0 +1,44 @@
+# import sys
+
+# def lower_bound(nums, target):
+#     left, right = 0, len(nums)
+
+#     while left < right:
+#         mid = (left + right) // 2
+#         if nums[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid
+#     return right
+
+# def upper_bound(nums, target):
+#     left, right = 0, len(nums)
+
+#     while left < right:
+#         mid = (left + right) // 2
+#         if nums[mid] <= target:
+#             left = mid + 1
+#         else:
+#             right = mid
+#     return right
+
+# n = int(sys.stdin.readline())
+# nl = list(map(int, sys.stdin.readline().split()))
+
+# m = int(sys.stdin.readline())
+# ml = list(map(int, sys.stdin.readline().split()))
+
+# nl.sort()
+# for i in ml:
+#     print(upper_bound(nl, i) - lower_bound(nl, i), end=' ')
+
+import sys
+import bisect
+
+n = int(sys.stdin.readline())
+nl = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+ml = list(map(int, sys.stdin.readline().split()))
+nl.sort()
+for i in ml:
+    print(bisect.bisect_right(nl, i) - bisect.bisect_left(nl, i), end=' ')
