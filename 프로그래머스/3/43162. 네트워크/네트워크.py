40310901ba456b38314e4aaa060f244c42df24a6
Yeonho Lee <52403373+CUAGAIN-95@users.noreply.github.com>
2024-10-02 20:57:59 +0900

    [level 3] Title: 네트워크, Time: 62.86 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def bfs(graph, start_v):
+    visited = [start_v]
+    queue = deque()
+    queue.append(start_v)
+    while queue:
+        cur_v = queue.popleft()
+        for v in graph[cur_v]:
+            if v not in visited:
+                visited.append(v)
+                queue.append(v)
+    return visited
+
+
+    
+    # return len(network)
+
+def solution(n, computers):
+    graph = {}
+    for i in range(n):
+        graph[i] = []
+        for j in range(n):
+            node = computers[i][j]
+            if i != j and node == 1:
+                graph[i].append(j)
+    network = []
+    for i in range(n):
+        net = set(bfs(graph, i))
+        if net not in network:
+            network.append(net)
+    
+    answer = len(network)
+    return answer
